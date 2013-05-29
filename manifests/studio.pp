32e692404805fc95b538b478996cbe8cf084dc9f
Ernad Husremović <hernad@bring.out.ba>
2013-05-29 14:35:06 +0200

    Update studio.pp

--- manifests/studio.pp
+++ manifests/studio.pp
@@ -4,7 +4,7 @@
 # Usage:
 #
 #     include android::studio
-class android::studio($version = '130.677228') {
+class android::studio($version = '130.687321') {
 
   package { "Android Studio Bundle ${version}":
     provider => 'appdmg',
